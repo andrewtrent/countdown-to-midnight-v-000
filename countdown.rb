789cda3ddd3dc7dac159fd98cb423a1c02b88d26
Andrew Trent <andrewtrentdesign@gmail.com>
2018-09-26 15:00:59 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,4 +5,14 @@ def countdown(int)
     puts "#{int} SECOND(S)!"
     int -= 1
   end
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  while int > 0
+    sleep(1)
+    puts "#{int} SECOND(S)!"
+    int -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
